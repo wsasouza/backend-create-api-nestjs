8a2d85f09415403c8ed6db75b1d23af87bcd8a8e
wsasouza <wsasouza@hotmail.com>
2021-12-08 10:11:09 -0300

    Feat: Create insert/list endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BreedsModule } from './modules/breeds/breeds.module';
 
 @Module({
-  imports: [BreedsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db/dogbreed.db',
+      entities: ['dist/**/**.entity{.ts,.js}'],
+      synchronize: false,
+    }),
+    BreedsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/modules/breeds/breeds.controller.ts
+++ src/modules/breeds/breeds.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { DogBreed } from 'src/database/entities/DogBreed.entity';
+import { BreedsService } from './breeds.service';
+import { CreateBreedDto } from './dtos/create-breed.dto';
 
 @Controller('breeds')
 export class BreedsController {
+  constructor(private readonly breedsService: BreedsService) {}
+
   @Get()
-  findAll(): string {
-    return 'List all breeds';
+  findAll(): Promise<DogBreed[]> {
+    return this.breedsService.findAll();
+  }
+
+  @Post()
+  create(@Body() dto: CreateBreedDto) {
+    this.breedsService.create(dto);
   }
 }
